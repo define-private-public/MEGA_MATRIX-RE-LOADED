ec9d42b5a1317937be52e52dc456b67b2ad3c0ee
Ben <define-private-public@users.noreply.github.com>
2014-08-25 17:47:11 -0400

    Added pins controls

--- mm_arduino/mm_arduino.ino
+++ mm_arduino/mm_arduino.ino
@@ -8,7 +8,7 @@
 // We Have to do this:
 //           Cols:|   1   2   3   4 
 //  Rows:         |
-------------------+-----------------
+// ---------------+-----------------
 // (n-1):     0   |   x   x   x   x
 // (n):       1   |   0   0   1   0
 // (n+1):     0   |   x   x   x   x
@@ -17,8 +17,38 @@
 // 0 = LOW
 // 1 = HIGH
 
-
 // Row: Acts as the emmiter
-
+int rData = 2;
+int rClock = 3;
+int rLatch = 4;
+int rEnable = 5;
+int rReset = 6;
 
 // Colum: Acts as the Reicver
+int cData = 8;
+int cClock = 9;
+int cLatch = 10;
+int cEnable = 11;
+int cReset = 12;
+
+// NOTE Could consolidate the Reset and enable pins possibly
+//      as well as the latch.  Test this later.
+
+
+void setup() {
+  // Set everything to output
+  pinMode(rData, OUTPUT);
+  pinMode(rClock, OUTPUT);
+  pinMode(rLatch, OUTPUT);
+  pinMode(rEnable, OUTPUT);
+  pinMode(rReset, OUTPUT);
+  pinMode(cData, OUTPUT);
+  pinMode(cClock, OUTPUT);
+  pinMode(cLatch, OUTPUT);
+  pinMode(cEnable, OUTPUT);
+  pinMode(cReset, OUTPUT);
+}
+
+void loop() {
+  
+}
