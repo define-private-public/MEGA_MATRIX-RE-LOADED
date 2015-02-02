1eb0e087a4682b0e5d9a73804c844eb7e3c52df6
Ben <define-private-public@users.noreply.github.com>
2015-02-02 21:32:32 +0000

    Working on first version

--- mm_pi/wiringPi.c
+++ mm_pi/wiringPi.c
@@ -1,5 +1,7 @@
 // Mega Matrix: A 24x24 LED Matrix
 //
+// WiringPi edition
+//
 // Our matrices are Command cathode; a 3x3 array of 8x8 matrices.
 // The row controllers will act as emitters, and the columns as
 // receivers.  (Using 74HC595 shift registers)
@@ -24,45 +26,49 @@
 // 0 = LOW
 // 1 = HIGH
 
+#define bool unsigned char
+#define true 1
+#define false 0
+
 
 // Row: Acts as the emmiter, all pins on port D of atmega
-int rData = 2;      // PD2
-int rClock = 3;     // PD3
-int rLatch = 4;     // PD4
-int rEnable = 5;    // PD5
-int rReset = 6;     // PD6
+int rData = 21;
+int rClock = 22;
+int rLatch = 23;
+int rEnable = 24;
+int rReset = 25;
 
 // Colunm: Acts as the Reicver, all pins on port B of atmega
-int cData = 8;      // PB0
-int cClock = 9;     // PB1
-int cLatch = 10;    // PB2
-int cEnable = 11;   // PB3
-int cReset = 12;    // PB4
+int cData = 0;
+int cClock = 2;
+int cLatch = 3;
+int cEnable = 4;
+int cReset = 5;
 
 // The need for speed:
 // For the Row controller
-#define R_DATA_HIGH PORTD |= _BV(PD2)
-#define R_DATA_LOW PORTD &= ~_BV(PD2)
-#define R_CLOCK_HIGH PORTD |= _BV(PD3)
-#define R_CLOCK_LOW PORTD &= ~_BV(PD3)
-#define R_LATCH_HIGH PORTD |= _BV(PD4)
-#define R_LATCH_LOW PORTD &= ~_BV(PD4)
-#define R_ENABLE_HIGH PORTD |= _BV(PD5)
-#define R_ENABLE_LOW PORTD &= ~_BV(PD5)
-#define R_RESET_HIGH PORTD |= _BV(PD6)
-#define R_RESET_LOW PORTD &= ~_BV(PD6)
+#define R_DATA_HIGH digitalWrite(rData, HIGH)
+#define R_DATA_LOW digitalWrite(rData, LOW)
+#define R_CLOCK_HIGH digitalWrite(rClock, HIGH)
+#define R_CLOCK_LOW digitalWrite(rClock, LOW)
+#define R_LATCH_HIGH digitalWrite(rLatch, HIGH)
+#define R_LATCH_LOW digitalWrite(rLatch, LOW)
+#define R_ENABLE_HIGH digitalWrite(rEnable, HIGH)
+#define R_ENABLE_LOW digitalWrite(rEnable, LOW)
+#define R_RESET_HIGH digitalWrite(rReset, HIGH)
+#define R_RESET_LOW digitalWrite(rReset, LOW)
 
 // For the Column controller
-#define C_DATA_HIGH PORTB |= _BV(PB0)
-#define C_DATA_LOW PORTB &= ~_BV(PB0)
-#define C_CLOCK_HIGH PORTB |= _BV(PB1)
-#define C_CLOCK_LOW PORTB &= ~_BV(PB1)
-#define C_LATCH_HIGH PORTB |= _BV(PB2)
-#define C_LATCH_LOW PORTB &= ~_BV(PB2)
-#define C_ENABLE_HIGH PORTB |= _BV(PB3)
-#define C_ENABLE_LOW PORTB &= ~_BV(PB3)
-#define C_RESET_HIGH PORTB |= _BV(PB4)
-#define C_RESET_LOW PORTB &= ~_BV(PB4)
+#define C_DATA_HIGH digitalWrite(cData, HIGH)
+#define C_DATA_LOW digitalWrite(cData, LOW)
+#define C_CLOCK_HIGH digitalWrite(cClock, HIGH)
+#define C_CLOCK_LOW digitalWrite(cClock, LOW)
+#define C_LATCH_HIGH digitalWrite(cLatch, HIGH)
+#define C_LATCH_LOW digitalWrite(cLatch, LOW)
+#define C_ENABLE_HIGH digitalWrite(cEnable, HIGH)
+#define C_ENABLE_LOW digitalWrite(cEnable, LOW)
+#define C_RESET_HIGH digitalWrite(cReset, HIGH)
+#define C_RESET_LOW digitalWrite(cReset, LOW)
 
 // NOTE Could consolidate the Reset and enable pins possibly
 //      as well as the latch.  Test this later.
@@ -74,73 +80,106 @@ int cReset = 12;    // PB4
 #define NUM_BYTES 72  // 24 * 3
 #define NUM_ROWS 24
 #define NUM_COLS 24
-boolean bufferReady = false;
-byte bytesRead = 0;
-byte image[NUM_BYTES];           // Image that's currently being displayed
-byte image_buffer[NUM_BYTES];    // Buffer for next image
+bool bufferReady = false;
+unsigned char bytesRead = 0;
+unsigned char image[NUM_BYTES];     // Image that's currently being displayed
+unsigned char image_buffer[NUM_BYTES];    // Buffer for next image
 
 
-// Some patterns
-byte CSH_logo[NUM_BYTES] = {
-   B00000000, B00000000, B00000000,
-   B00111111, B11111111, B10011110,
-   B01111111, B11111111, B11011110,
-   B01111111, B11111111, B11011110,
-   B01111111, B11111111, B11011110,
-   B01111000, B00000011, B11011110,
-   B01111011, B11111011, B11011110,
-   B01111011, B11111000, B00011110,
-   B01111011, B11111011, B11011110,
-   B01111011, B10111011, B11011110,
-   B01111011, B10000011, B11111110,
-   B01111011, B11111011, B11111110,
-   B01111011, B11111011, B11111110,
-   B01111000, B00111011, B11111110,
-   B01111011, B10111011, B11011110,
-   B01111011, B11111011, B11011110,
-   B01111011, B11111000, B00011110,
-   B01111011, B11111011, B11011110,
-   B01111000, B00000011, B11011110,
-   B01111111, B11111111, B11011110,
-   B01111111, B11111111, B11011110,
-   B01111111, B11111111, B11011110,
-   B00111111, B11111111, B10011110,
-   B00000000, B00000000, B00000000,
-};
+//// Some patterns
+//unsigned char CSH_logo[NUM_BYTES] = {
+//   B00000000, B00000000, B00000000,
+//   B00111111, B11111111, B10011110,
+//   B01111111, B11111111, B11011110,
+//   B01111111, B11111111, B11011110,
+//   B01111111, B11111111, B11011110,
+//   B01111000, B00000011, B11011110,
+//   B01111011, B11111011, B11011110,
+//   B01111011, B11111000, B00011110,
+//   B01111011, B11111011, B11011110,
+//   B01111011, B10111011, B11011110,
+//   B01111011, B10000011, B11111110,
+//   B01111011, B11111011, B11111110,
+//   B01111011, B11111011, B11111110,
+//   B01111000, B00111011, B11111110,
+//   B01111011, B10111011, B11011110,
+//   B01111011, B11111011, B11011110,
+//   B01111011, B11111000, B00011110,
+//   B01111011, B11111011, B11011110,
+//   B01111000, B00000011, B11011110,
+//   B01111111, B11111111, B11011110,
+//   B01111111, B11111111, B11011110,
+//   B01111111, B11111111, B11011110,
+//   B00111111, B11111111, B10011110,
+//   B00000000, B00000000, B00000000,
+//};
 
-byte test_pattern[NUM_BYTES] = {
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
-  B01010101, B01010101, B01010101,
-  B10101010, B10101010, B10101010,
+unsigned char test_pattern[NUM_BYTES] = {
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+//  B01010101, B01010101, B01010101,
+//  B10101010, B10101010, B10101010,
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z',
+	'z', 'z', 'z'
 };
 
 
 
 void clsCols() {
   // Clears out all of the columns (Turns them off by setting their values to 1)
+  int c;
   C_DATA_HIGH;
-  for (int c = 0; c < NUM_COLS; c++) {
+  for (c = 0; c < NUM_COLS; c++) {
     C_CLOCK_HIGH;
     C_CLOCK_LOW;
   }
@@ -159,7 +198,7 @@ void clsRows() {
 
 
 void setup() {
-  Serial.begin(115200);
+	int i;
   
   // Set everything to output
   pinMode(rData, OUTPUT);
@@ -179,9 +218,9 @@ void setup() {
   R_RESET_HIGH;
   C_RESET_HIGH;
   
-//  // XOR the whole image (pre-process)
-//  for (int i = 0; i < (24 * 3); i++)
-//    image[i] = test_pattern[i];
+  // XOR the whole image (pre-process)
+  for (i = 0; i < (24 * 3); i++)
+    image[i] = test_pattern[i];
 
   // Start with a clean slate
   R_ENABLE_HIGH;
@@ -195,34 +234,19 @@ void setup() {
 
 void loop() {  
 //  unsigned long start = micros();
+	// Vars
+	int r, c, row, b;
   
-  // This is not using buffered IO like it should, will figure that out later
-  byte bytesReady = Serial.available();
-  while (bytesReady > 0) {
-    // Get the next byte
-    
-    image_buffer[bytesRead] = (byte)Serial.read();
-    
-    bytesRead += 1;
-    
-    if (bytesRead == 72) {
-      // Read in a whole image, set it to display
-      memcpy(image, image_buffer, bytesRead);
-      bytesRead = 0;
-      bytesReady = 0;
-    } else
-      bytesReady = Serial.available();  // Loop again
-  }
   
   // Put up the image
-  for (int r = 0; r < NUM_ROWS; r += 2) {
+  for (r = 0; r < NUM_ROWS; r += 2) {
 //    unsigned long start = micros();
-    int row = r * 3;
+    row = r * 3;
     
     // Put up the column
-    for (int c = 2; c >= 0; c--) {      
+    for (c = 2; c >= 0; c--) {      
       // Do a shift out, LSB First
-      for (int b = 0; b < 8; b++) {
+      for (b = 0; b < 8; b++) {
         // If a bit is 1, make it HIGH, else LOW
         if (image[row + c] & (1 << b))
           C_DATA_LOW;
@@ -291,6 +315,19 @@ void loop() {
 //  R_CLOCK_LOW;
 //  R_LATCH_LOW;
 //  R_LATCH_HIGH;
-  
-//  Serial.println(micros() - start);
+}
+
+
+int main(int argc, char *argv[]) {
+	int i;
+
+	wiringPiSetup();
+	setup();
+
+	for (i = 0; i < 5; i++) {
+		loop();
+		delay(1000);
+	}
+
+	return 0;
 }
