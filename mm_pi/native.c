cc2e0bf79c1e3ab569f4b394f0b4ca25be579631
Ben <define-private-public@users.noreply.github.com>
2015-02-06 20:10:39 +0000

    Gives us 19K FPS w/ -O3

--- mm_pi/native.c
+++ mm_pi/native.c
@@ -21,11 +21,13 @@
 // (n-1):     0   |   x   x   x   x
 // (n):       1   |   0   0   1   0
 // (n+1):     0   |   x   x   x   x
-
+//
 // x = Don't care
 // 0 = LOW
 // 1 = HIGH
 
+
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -33,11 +35,13 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-#define BCM2708_PERI_BASE   0x20000000
-#define GPIO_BASE           (BCM2708_PERI_BASE + 0x200000) /* GPIO Controler */
+// Raspberry Pi stuff
+#define BCM2708_PERI_BASE 0x20000000
+#define GPIO_BASE (BCM2708_PERI_BASE + 0x200000) /* GPIO Controler */
 #define PAGE_SIZE (4 * 1024)
 #define BLOCK_SIZE (4 * 1024)
 
+// Give us bools
 #define bool unsigned char
 #define true 1
 #define false 0
@@ -60,25 +64,14 @@ volatile unsigned *gpio;
 #define GPIO_PULLCLK0 *(gpio + 38) 	// Pull up/pull down clock
 
 
-//// Row: Acts as the emmiter, all pins on port D of atmega
-//int rData = 5;
-//int rClock = 6;
-//int rLatch = 13;
-//int rEnable = 19;
-//int rReset = 26;
-//
-//// Colunm: Acts as the Reicver, all pins on port B of atmega
-//int cData = 17;
-//int cClock = 27;
-//int cLatch = 22;
-//int cEnable = 23;
-//int cReset = 24;
+// Row: Acts as the emmiter, all pins on port D of atmega
 #define rData 5
 #define rClock 6
 #define rLatch 13
 #define rEnable 19
 #define rReset 26
 
+// Colunm: Acts as the Reicver, all pins on port B of atmega
 #define cData 17
 #define cClock 27
 #define cLatch 22
@@ -112,12 +105,6 @@ int pins[10];
 #define C_RESET_HIGH GPIO_SET = 1 << cReset
 #define C_RESET_LOW GPIO_CLR = 1 << cReset
 
-// NOTE Could consolidate the Reset and enable pins possibly
-//      as well as the latch.  Test this later.
-
-
-
-
 
 #define NUM_BYTES 72  // 24 * 3
 #define NUM_ROWS 24
@@ -137,57 +124,33 @@ void setup();
 
 //// Some patterns
 //unsigned char CSH_logo[NUM_BYTES] = {
-//   B00000000, B00000000, B00000000,
-//   B00111111, B11111111, B10011110,
-//   B01111111, B11111111, B11011110,
-//   B01111111, B11111111, B11011110,
-//   B01111111, B11111111, B11011110,
-//   B01111000, B00000011, B11011110,
-//   B01111011, B11111011, B11011110,
-//   B01111011, B11111000, B00011110,
-//   B01111011, B11111011, B11011110,
-//   B01111011, B10111011, B11011110,
-//   B01111011, B10000011, B11111110,
-//   B01111011, B11111011, B11111110,
-//   B01111011, B11111011, B11111110,
-//   B01111000, B00111011, B11111110,
-//   B01111011, B10111011, B11011110,
-//   B01111011, B11111011, B11011110,
-//   B01111011, B11111000, B00011110,
-//   B01111011, B11111011, B11011110,
-//   B01111000, B00000011, B11011110,
-//   B01111111, B11111111, B11011110,
-//   B01111111, B11111111, B11011110,
-//   B01111111, B11111111, B11011110,
-//   B00111111, B11111111, B10011110,
-//   B00000000, B00000000, B00000000,
+//	 B00000000, B00000000, B00000000,
+//	 B00111111, B11111111, B10011110,
+//	 B01111111, B11111111, B11011110,
+//	 B01111111, B11111111, B11011110,
+//	 B01111111, B11111111, B11011110,
+//	 B01111000, B00000011, B11011110,
+//	 B01111011, B11111011, B11011110,
+//	 B01111011, B11111000, B00011110,
+//	 B01111011, B11111011, B11011110,
+//	 B01111011, B10111011, B11011110,
+//	 B01111011, B10000011, B11111110,
+//	 B01111011, B11111011, B11111110,
+//	 B01111011, B11111011, B11111110,
+//	 B01111000, B00111011, B11111110,
+//	 B01111011, B10111011, B11011110,
+//	 B01111011, B11111011, B11011110,
+//	 B01111011, B11111000, B00011110,
+//	 B01111011, B11111011, B11011110,
+//	 B01111000, B00000011, B11011110,
+//	 B01111111, B11111111, B11011110,
+//	 B01111111, B11111111, B11011110,
+//	 B01111111, B11111111, B11011110,
+//	 B00111111, B11111111, B10011110,
+//	 B00000000, B00000000, B00000000,
 //};
 
 unsigned char test_pattern[NUM_BYTES] = {
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
-//  B01010101, B01010101, B01010101,
-//  B10101010, B10101010, B10101010,
 	'a', 'b', 'c',
 	'b', 'c', 'd',
 	'c', 'd', 'e',
@@ -216,29 +179,30 @@ unsigned char test_pattern[NUM_BYTES] = {
 
 
 void clsCols() {
-  // Clears out all of the columns (Turns them off by setting their values to 1)
-  int c;
-  C_DATA_HIGH;
-  for (c = 0; c < NUM_COLS; c++) {
-    C_CLOCK_HIGH;
-    C_CLOCK_LOW;
-  }
-  C_DATA_LOW;
-    
-  C_LATCH_HIGH;
-  C_LATCH_LOW;
+	// Clears out all of the columns (Turns them off by setting their values to 1)
+	int c;
+	C_DATA_HIGH;
+	for (c = 0; c < NUM_COLS; c++) {
+		C_CLOCK_HIGH;
+		C_CLOCK_LOW;
+	}
+	C_DATA_LOW;
+		
+	C_LATCH_HIGH;
+	C_LATCH_LOW;
 }
 
 
 void clsRows() {
-  // Clear out the shift register
-  R_RESET_LOW;
-  R_RESET_HIGH;
+	// Clear out the shift register
+	R_RESET_LOW;
+	R_RESET_HIGH;
 }
 
 
 // Setup memory regions to access GPIO
 void setup_gpio() {
+	// Init the pin list
 	pins[0] = rData;
 	pins[1] = rClock;
 	pins[2] = rLatch;
@@ -281,126 +245,115 @@ void setup_gpio() {
 void setup() {
 	int i;
 	int p;
-  
-//  // Set everything to output
-//  pinMode(rData, OUTPUT);
-//  pinMode(rClock, OUTPUT);
-//  pinMode(rLatch, OUTPUT);
-//  pinMode(rEnable, OUTPUT);
-//  pinMode(rReset, OUTPUT);
-//  pinMode(cData, OUTPUT);
-//  pinMode(cClock, OUTPUT);
-//  pinMode(cLatch, OUTPUT);
-//  pinMode(cEnable, OUTPUT);
-//  pinMode(cReset, OUTPUT);
-
+	
+	// Set everything to output
 	for (p = 0; p < 10; p++) {
 		INP_GPIO(pins[p]);
 		OUT_GPIO(pins[p]);
 		GPIO_CLR = 1 << pins[p];			// SET it LOW
 	}
-  
-  // Turn on output, disable reset
-  R_ENABLE_LOW;
-  C_ENABLE_LOW;
-  R_RESET_HIGH;
-  C_RESET_HIGH;
-  
-  // XOR the whole image (pre-process)
-  for (i = 0; i < (24 * 3); i++)
-    image[i] = test_pattern[i];
+	
+	// Turn on output, disable reset
+	R_ENABLE_LOW;
+	C_ENABLE_LOW;
+	R_RESET_HIGH;
+	C_RESET_HIGH;
+	
+	// XOR the whole image (pre-process)
+	for (i = 0; i < (24 * 3); i++)
+		image[i] = test_pattern[i];
 
-  // Start with a clean slate
-  R_ENABLE_HIGH;
-  C_ENABLE_HIGH;
-  clsRows();
-  clsCols();
-  C_ENABLE_LOW;
-  R_ENABLE_LOW;
+	// Start with a clean slate
+	R_ENABLE_HIGH;
+	C_ENABLE_HIGH;
+	clsRows();
+	clsCols();
+	C_ENABLE_LOW;
+	R_ENABLE_LOW;
 }
 
 
-void display() {  
-//  unsigned long start = micros();
+void display() {	
+//	unsigned long start = micros();
 	// Vars
 	int r, c, row, b;
 	unsigned char subRow;
-  
-  
-  // Put up the image
-  for (r = 0; r < NUM_ROWS; r += 2) {
-//    unsigned long start = micros();
-    row = r * 3;
-    
-    // Put up the column
-    for (c = 2; c >= 0; c--) {      
-	  subRow = image[row + c];
+	
+	
+	// Put up the image
+	for (r = 0; r < NUM_ROWS; r += 2) {
+//		unsigned long start = micros();
+		row = r * 3;
+		
+		// Put up the column
+		for (c = 2; c >= 0; c--) {			
+		subRow = image[row + c];
 
-      // Do a shift out, LSB First
-      for (b = 0; b < 8; b++) {
-        // If a bit is 1, make it HIGH, else LOW
-        if (subRow & (1 << b))
-          C_DATA_LOW;
-        else
-          C_DATA_HIGH;
-        
-        // Send the bit to the shift register
-        C_CLOCK_HIGH;
-        C_CLOCK_LOW;
-      }
-    }
-    
-    // the Row (need if block because of interlacting)
-    if (r == 0) {
-      // First pass, row 1 should be high, row 2 low
-      R_DATA_LOW;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-      R_DATA_HIGH;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-    } else if (r == 1) {
-      // Second pass, row 1 low, row 2 high
-      R_DATA_HIGH;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-      R_DATA_LOW;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-    } else {
-      // Shift down by two
-      // Everything else should be low
-      R_DATA_LOW;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-      R_DATA_LOW;
-      R_CLOCK_HIGH;
-      R_CLOCK_LOW;
-    }
-    
-    // Send the buffers to output
-    R_ENABLE_HIGH;
-    C_ENABLE_HIGH;
-    
-    R_LATCH_LOW;
-    R_LATCH_HIGH;
-    C_LATCH_LOW;
-    C_LATCH_HIGH;
-    
-    C_ENABLE_LOW;
-    R_ENABLE_LOW;
-    
-    // for interlacing
-    if (r == (NUM_ROWS - 2))
-      r = -1;
-  }
-  
-//  NOTE: This code was meant to clear out the last row, not sure if necessary or not.
-//  R_DATA_LOW;
-//  R_CLOCK_HIGH;
-//  R_CLOCK_LOW;
-//  R_LATCH_LOW;
-//  R_LATCH_HIGH;
+			// Do a shift out, LSB First
+			for (b = 0; b < 8; b++) {
+				// If a bit is 1, make it HIGH, else LOW
+				if (subRow & (1 << b))
+					C_DATA_LOW;
+				else
+					C_DATA_HIGH;
+				
+				// Send the bit to the shift register
+				C_CLOCK_HIGH;
+				C_CLOCK_LOW;
+			}
+		}
+		
+		// the Row (need if block because of interlacting)
+		if (r == 0) {
+			// First pass, row 1 should be high, row 2 low
+			R_DATA_LOW;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+			R_DATA_HIGH;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+		} else if (r == 1) {
+			// Second pass, row 1 low, row 2 high
+			R_DATA_HIGH;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+			R_DATA_LOW;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+		} else {
+			// Shift down by two
+			// Everything else should be low
+			R_DATA_LOW;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+			R_DATA_LOW;
+			R_CLOCK_HIGH;
+			R_CLOCK_LOW;
+		}
+		
+		// Send the buffers to output
+		R_ENABLE_HIGH;
+		C_ENABLE_HIGH;
+		
+		R_LATCH_LOW;
+		R_LATCH_HIGH;
+		C_LATCH_LOW;
+		C_LATCH_HIGH;
+		
+		C_ENABLE_LOW;
+		R_ENABLE_LOW;
+		
+		// for interlacing
+		if (r == (NUM_ROWS - 2))
+			r = -1;
+	}
+	
+	//NOTE: This code was meant to clear out the last row, not sure if necessary or not.
+//	R_DATA_LOW;
+//	R_CLOCK_HIGH;
+//	R_CLOCK_LOW;
+//	R_LATCH_LOW;
+//	R_LATCH_HIGH;
 }
 
 
