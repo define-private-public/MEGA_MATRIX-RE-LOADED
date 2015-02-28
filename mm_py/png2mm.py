1a1655b3155620ce1afe63764038f78f6f6013bd
Ben <define-private-public@users.noreply.github.com>
2015-02-28 11:52:00 -0500

    png2mm now works independant of 24x24 size

--- mm_py/png2mm.py
+++ mm_py/png2mm.py
@@ -2,16 +2,16 @@
 #
 # Small little application that will Take in a Black and White PNG image, a
 # amount of seconds to display the sequence, and a destianion direction.
-# Image must be exactly 24 pixels high.  For a pixel to be on, paint white for
-# off, paint black.  Must also be minimu 24 pixels wide
+# Image must be NxM pixels, N and M are specified in the command line arguments
+# as <RxC>. It's recommended to keep them as positive multiples of 24.
 #
 # If any of the values are bad, no image will get saved
 #
 # Usage:
-#   python3 png2mm.py <png_file> <seconds> <destination>
+#   python3 png2mm.py <RxC> <png_file> <seconds> <destination>
 #
 # Example:
-#   python3 png2mm.py banner.png 4.5 banner_message
+#   python3 png2mm.py 24x48 banner.png 4.5 banner_message
 
 import sys, os
 from PySide.QtGui import *
@@ -24,19 +24,27 @@ def main():
 
 	# Get some values
 	argc = len(sys.argv)
-	if argc < 4:
+	if argc < 5:
 		# Exit
 		print('Need at least three arguements.')
 		print('Usage:')
-		print('  python3 mm2png.py <png_file> <seconds> <destination>')
+		print('  python3 mm2png.py <RxC> <png_file> <seconds> <destination>')
 		sys.exit(0)
 	
 	# Peel some data out
-	pngFilename = sys.argv[1]
-	seconds = float(sys.argv[2])
-	dest = sys.argv[3]
+	dim = sys.argv[1].split('x')
+	pngFilename = sys.argv[2]
+	seconds = float(sys.argv[3])
+	dest = sys.argv[4]
+	dimRows = 0
+	dimCols = 0
+
+
 
 	# Checks
+	if len(dim) != 2:
+		print('Need an NxM value, that is positive integers')
+
 	if not pngFilename[-4:].lower().endswith('.png'):
 		print('Image needs to be a PNG file.')
 		sys.exit(0)
@@ -45,13 +53,16 @@ def main():
 		print('Seconds needs to be a positive real number.')
 		sys.exit(0)
 	
+	# Get the dimensions
+	dimRows = int(dim[0])
+	dimCols = int(dim[1])
+	
 	# Read in the Image data
 	png = QImage(pngFilename)
-	size = 24
 	width = png.width()
 
 	# Size check
-	if width < 24:
+	if width < dimCols:
 		print('PNG width too small')
 		sys.exit(1)
 
@@ -61,12 +72,12 @@ def main():
 	# Read the stuff into text files
 	col = 1
 	blackPixel = qRgb(0x00, 0x00, 0x00)
-	while ((col -  1) + size) <= width:
+	while ((col -  1) + dimCols) <= width:
 		textFile = open('%s/%i.txt'%(dest, col), 'w')
 
 		# read pixels
-		for r in range(0, size):
-			for c in range(col - 1, (col - 1) +size):
+		for r in range(0, dimRows):
+			for c in range(col - 1, (col - 1) + dimCols):
 				# inspect pixel
 				pixel = png.pixel(c, r)
 				if pixel == blackPixel:
